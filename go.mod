6b770210c5bd94f5f1dc1f4b375fadea8542dc9b
pochita <alonso.coaguila@epfl.ch>
2025-01-28 17:57:38 +0100

    feat: create main + go.mod + go.sum

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-doit
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Todo struct {
+	Id    string
+	User  string
+	Title string
+	Task  string
+	Done  bool
+}
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Welcome to go-doit!")
+
+	for {
+		fmt.Println("Choose what to do: (1: Create Todo) (2: Check Todo)")
+		input, _ := reader.ReadString('\n')
+		req := strings.TrimRight(input, "\n")
+
+		if req == "q" {
+			break
+		}
+		fmt.Println(req)
+	}
+
+}
